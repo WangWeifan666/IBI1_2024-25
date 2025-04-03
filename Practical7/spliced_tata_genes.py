82fc5b91969ca413d5d29ce68b906f691f1c03aa
Welfie <3789792675@qq.com>
2025-04-03 12:24:56 +0800

    4.3

--- Practical7/spliced_tata_genes.py
+++ Practical7/spliced_tata_genes.py
@@ -5,13 +5,14 @@ spiked_combination = input("Enter splice donor/acceptor combination (GTAG, GCAG,
 while spiked_combination not in valid_combinations:
     print("Invalid input! Please choose from GTAG, GCAG, ATAC.")
     spiked_combination = input("Enter combination: ").strip().upper()
-
+# Process the spliced genes
+# Expression for TATA box pattern
 tata_pattern = r"TATA[AT]A[AT]"
 input_path = r"C:\Users\ASUS\Desktop\Github\IBI1_2024-25\Practical7\Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa"
 output_filename = fr"C:\Users\ASUS\Desktop\Github\IBI1_2024-25\Practical7\{spiked_combination}_spliced_genes.fa"
-
+# Sequence Processing
 with open(input_path, 'r') as infile, open(output_filename,'w') as outfile:
-    gene_name = None
+    gene_name = None #Track current gene name
     sequence = []
     for line in infile:
         if line.startswith('>'):
@@ -20,11 +21,11 @@ with open(input_path, 'r') as infile, open(output_filename,'w') as outfile:
                     tata_count = len(re.findall(tata_pattern, ''.join(sequence)))
                     outfile.write(f'>{gene_name} TATA_count={tata_count}\n')
                     outfile.write(''.join(sequence) + '\n')
-            gene_name = line.split()[3][5:]  # 根据实际文件格式调整索引
+            gene_name = line.split()[3][5:]  # Extract gene name
             sequence = []
         else:
             sequence.append(line.strip())
-    # 处理最后一个基因
+    # Check the last gene
     if gene_name and re.search(tata_pattern, ''.join(sequence)):
        if re.search(fr'{spiked_combination}',''.join(sequence)):
             tata_count = len(re.findall(tata_pattern, ''.join(sequence)))
