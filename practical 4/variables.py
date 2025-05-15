c513a067c40182964fbf69667701c2560e78449f
WangWeifan666 <weifan.24@intl.zju.edu.cn>
2025-05-15 21:59:26 +0800

    Add files via upload

--- practical 4/variables.py
+++ practical 4/variables.py
@@ -7,9 +7,25 @@ f=d+e
 print(c)
 print(f)
 #f>c
-#driving is faster
+if c > f:
+    print("Driving is faster")
+elif c == f:
+    print("The time for the two transport is the same")
+else:
+    print("Taking bus is quicker")
+#Taking bus is faster
 X=True
 Y=False
 W=X and Y
 print(W)
-#W=False
+
+'''
+Truth table for W:
+X     | Y     | W
+------------------------------------------------
+True  | True  | True
+True  | False | False
+False | True  | False
+False | False | False
+'''
+
