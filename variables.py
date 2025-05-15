80483b6dd9edb7809e1cdb85e4b4b46aebbb226c
WangWeifan666 <weifan.24@intl.zju.edu.cn>
2025-05-15 21:57:54 +0800

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,31 @@
+a=15
+b=75
+c=a+b
+d=90
+e=5
+f=d+e
+print(c)
+print(f)
+#f>c
+if c > f:
+    print("Driving is faster")
+elif c == f:
+    print("The time for the two transport is the same")
+else:
+    print("Taking bus is quicker")
+#Taking bus is faster
+X=True
+Y=False
+W=X and Y
+print(W)
+
+'''
+Truth table for W:
+X     | Y     | W
+------------------------------------------------
+True  | True  | True
+True  | False | False
+False | True  | False
+False | False | False
+'''
+
